08534b3398850199cf75fa258783ee6fc38602df
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-14 23:08:49 +0300

    edited BookStoreTests.java

--- src/test/java/com/bookstoreapi/tests/BookStoreTests.java
+++ src/test/java/com/bookstoreapi/tests/BookStoreTests.java
@@ -1,7 +1,18 @@
 package com.bookstoreapi.tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+import static com.bookstoreapi.helpers.CustomAllureListener.withCustomTemplates;
 
 public class BookStoreTests {
 
@@ -9,6 +20,35 @@ public class BookStoreTests {
     static void setup() {
         RestAssured.baseURI = "https://demoqa.com";
 
+    }
+
+    @Test
+    void getBookTest() {
+        given()
+                .filter(withCustomTemplates())
+                .params("ISBN", "978144932586288")//
+                .log().uri()
+                .log().body()
+                .when()
+                .get("/BookStore/v1/Book")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("message", is("ISBN supplied is not available in Books Collection!"));
+    }
+
+    @Test
+    void getBooksTest() {
+        given()
+                .filter(withCustomTemplates())
+                .log().all()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+                .log().all()
+                .body("books", hasSize(greaterThan(0)))
+                .body("books.title[0]", is("Git Pocket Guide"));
     }
 
 }
