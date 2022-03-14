08c03fa002a2abd734956a177f5b8534b71cc671
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-14 22:28:28 +0300

    edited BookStoreTests.java, added "resources" directory, added request.ftl and response.ftl within tpl directory

--- src/test/java/com/bookstoreapi/tests/BookStoreTests.java
+++ src/test/java/com/bookstoreapi/tests/BookStoreTests.java
@@ -1,4 +1,14 @@
 package com.bookstoreapi.tests;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
 public class BookStoreTests {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://demoqa.com";
+
+    }
+
 }
